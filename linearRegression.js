3fb01f56a074b8184295cf057bc8103a50c2d9e5
msanlisavas <80029111+msanlisavas@users.noreply.github.com>
2022-12-16 11:20:16 +0300

    Create linearRegression.js

---
+++ linearRegression.js
@@ -0,0 +1,39 @@
+function linearRegression(xVals, yVals) {
+    regression = {
+        rSquared: 0,
+        yIntercept: 0,
+        slope: 0,
+
+    }
+    var sumOfX = 0;
+    var sumOfY = 0;
+    var sumOfXSq = 0;
+    var sumOfYSq = 0;
+    var sumCodeviates = 0;
+
+    for (var i = 0; i < xVals.length; i++) {
+        var x = xVals[i];
+        var y = yVals[i];
+        sumCodeviates += x * y;
+        sumOfX += x;
+        sumOfY += y;
+        sumOfXSq += x * x;
+        sumOfYSq += y * y;
+    }
+    var count = xVals.length;
+    var ssX = sumOfXSq - ((sumOfX * sumOfX) / count);
+
+    var rNumerator = (count * sumCodeviates) - (sumOfX * sumOfY);
+    var rDenom = (count * sumOfXSq - (sumOfX * sumOfX)) * (count * sumOfYSq - (sumOfY * sumOfY));
+    var sCo = sumCodeviates - ((sumOfX * sumOfY) / count);
+
+    var meanX = sumOfX / count;
+    var meanY = sumOfY / count;
+    var dblR = rNumerator / Math.sqrt(rDenom);
+
+    regression.rSquared = dblR * dblR;
+    regression.yIntercept = meanY - ((sCo / ssX) * meanX);
+    regression.slope = sCo / ssX;
+    return regression;
+
+}
